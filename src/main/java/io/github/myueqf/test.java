c8f35f6c32e617ed382e4e94ad09c780da734130
myueqf <myueqf@gmail.com>
2025-08-15 16:05:12 +0800

    夹带私货QwQ

--- src/main/java/io/github/myueqf/test.java
+++ src/main/java/io/github/myueqf/test.java
@@ -1,6 +1,7 @@
 package io.github.myueqf;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
@@ -18,8 +19,16 @@ public class test implements ModInitializer {
                     Identifier.of(MOD_ID, "chunk_generator_2d"),
                     ChunkGenerator2D.CODEC
             );
-        LOGGER.info("    猫猫探头～");
-        LOGGER.info("(=^-ω-^=)   ");
-        LOGGER.info("============");
+            /* 夹带私货QwQ */
+        LOGGER.info("    __                  __                ___________ ___________");
+        LOGGER.info("   / /___ _____  ____  / /_  ____ _____  / ____/ ___// ____/ ___/");
+        LOGGER.info("  / / __ `/ __ \\/_  / / __ \\/ __ `/ __ \\/___ \\/ __ \\/___ \\/ __ \\");
+        LOGGER.info(" / / /_/ / / / / / /_/ / / / /_/ / / / /___/ / /_/ /___/ / /_/ /");
+        LOGGER.info("/_/\\__,_/_/ /_/ /___/_/ /_/\\__,_/_/ /_/_____/\\____/_____/\\____/");
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            LOGGER.info("     猫猫探头～");
+            LOGGER.info(" (=^-ω-^=)   ");
+            LOGGER.info("=============");
+        });
     }
 }
